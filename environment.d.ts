9d764214399532c21a01582f3240b34835ff0725
Dima Krutolianov <DimaKrutDK@ya.ru>
2023-06-27 06:55:46 +0200

    chore: add google maps api key env var

--- environment.d.ts
+++ environment.d.ts
@@ -3,12 +3,15 @@ declare global {
     interface ProcessEnv {
       PORT?: string
       NODE_ENV: 'development' | 'production' | 'test'
+
       GOOGLE_CLIENT_ID: string
       GOOGLE_CLIENT_SECRET: string
       SUPERTOKENS_CONNECTION_URI: string
       SUPERTOKENS_API_KEY: string
       NEXT_PUBLIC_FRONTEND_DOMAIN: string
       NEXT_PUBLIC_API_DOMAIN: string
+
+      GOOGLE_MAPS_API_KEY: string
     }
   }
 }
