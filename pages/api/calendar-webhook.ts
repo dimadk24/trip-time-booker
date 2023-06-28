926cddbbd6b0fc99288abcba4a6d10fc9d4c08ca
Dima Krutolianov <DimaKrutDK@ya.ru>
2023-06-28 21:55:33 +0200

    feat: calculating trip duration

--- pages/api/calendar-webhook.ts
+++ pages/api/calendar-webhook.ts
@@ -3,11 +3,15 @@ import { type Request, Response } from 'express'
 import ThirdPartyNode from 'supertokens-node/recipe/thirdparty'
 import { getCredentials } from '@/src/services/user-meta'
 import { getJustChangedEvents } from '@/src/services/google-calendar'
+import { getTripDuration } from '@/src/services/google-maps'
+import { HOME_LOCATION } from '@/src/config/app-config'
 import { createAppLogger } from '@/src/utils/logger'
 
 const INVALID_CHANNEL_TOKEN = 'Invalid channel token'
 const INVALID_RESOUCE_ID = 'Invalid x-goog-resource-id header'
 const INVALID_RESOURCE_STATE = 'Invalid x-goog-resource-state header'
+
+const TRIP_EVENT_GAP = 5 * 60 // 5 min
 
 const logger = createAppLogger('calendar-webhook')
 
@@ -59,11 +63,27 @@ export default async function calendarWebhook(
   const justCreatedEvents = await getJustChangedEvents(credentials, userId)
 
   const promises = justCreatedEvents.map(async (event) => {
+    const eventId = event.id
     if (!event.location) {
-      logger.debug('No location for event', { eventId: event.id, userId })
+      logger.debug('No location for event', { eventId, userId })
       return
     }
-    // get time from maps api
+    if (!event.start?.dateTime) {
+      logger.warn('No start datetime for event', { eventId, userId })
+      return
+    }
+
+    const arrivalTimestamp =
+      Date.parse(event.start.dateTime) / 1000 - TRIP_EVENT_GAP
+
+    const duration = await getTripDuration(
+      HOME_LOCATION,
+      event.location,
+      arrivalTimestamp,
+      userId
+    )
+    console.log(duration)
+
     // create events
   })
 
