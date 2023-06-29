f5ba31b4bd8bead0db6817a132e871e350025465
Dima Krutolianov <DimaKrutDK@ya.ru>
2023-06-29 07:33:49 +0200

    refactor: change google-calendar service to class

--- src/services/google-calendar.ts
+++ src/services/google-calendar.ts
@@ -12,72 +12,67 @@ export type Credentials = Parameters<
   InstanceType<(typeof google)['auth']['OAuth2']>['setCredentials']
 >[0]
 
-const createClient = async (credentials: Credentials) => {
-  const oauth2Client = new google.auth.OAuth2({
-    clientId: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    redirectUri: '',
-  })
+type CalendarClient = ReturnType<(typeof google)['calendar']>
 
-  oauth2Client.setCredentials(credentials)
+export class GoogleCalendarService {
+  client: CalendarClient
 
-  const calendar = google.calendar({
-    version: 'v3',
-    auth: oauth2Client,
-  })
-  return calendar
-}
+  constructor(credentials: Credentials) {
+    const oauth2Client = new google.auth.OAuth2({
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      redirectUri: '',
+    })
 
-export const registerWebhook = async (
-  userId: string,
-  credentials: Credentials
-) => {
-  logger.debug('Registering calendar webhook', { userId })
+    oauth2Client.setCredentials(credentials)
 
-  const calendar = await createClient(credentials)
-
-  const webhookURL = `${WEBHOOK_DOMAIN}/api/calendar-webhook`
-
-  const { data } = await calendar.events.watch({
-    calendarId: 'primary',
-    requestBody: {
-      id: 'main',
-      token: userId,
-      type: 'webhook',
-      address: webhookURL,
-    },
-  })
-  const { id, resourceId } = data
-
-  logger.debug('Calendar webhook registered', {
-    userId,
-    webhookResouceId: resourceId,
-    webhookURL,
-    webhookId: id,
-  })
-  return { id, resourceId }
-}
-
-export const getJustChangedEvents = async (
-  credentials: Credentials,
-  userId: string
-) => {
-  const calendar = await createClient(credentials)
-
-  const fiveMinBefore = new Date()
-  fiveMinBefore.setMinutes(fiveMinBefore.getMinutes() - 5)
-
-  const params = {
-    calendarId: 'primary',
-    timeMin: new Date().toISOString(),
-    updatedMin: fiveMinBefore.toISOString(),
-    showDeleted: false,
+    this.client = google.calendar({
+      version: 'v3',
+      auth: oauth2Client,
+    })
   }
 
-  logger.debug('Getting recently changed events', { userId, ...params })
-  const response = await calendar.events.list(params)
-  const items = response.data.items || []
-  logger.info('Got recently changed events', { count: items.length, userId })
+  async registerWebhook(userId: string) {
+    logger.debug('Registering calendar webhook', { userId })
 
-  return items
+    const webhookURL = `${WEBHOOK_DOMAIN}/api/calendar-webhook`
+
+    const { data } = await this.client.events.watch({
+      calendarId: 'primary',
+      requestBody: {
+        id: 'main',
+        token: userId,
+        type: 'webhook',
+        address: webhookURL,
+      },
+    })
+    const { id, resourceId } = data
+
+    logger.debug('Calendar webhook registered', {
+      userId,
+      webhookResouceId: resourceId,
+      webhookURL,
+      webhookId: id,
+    })
+    return { id, resourceId }
+  }
+
+  async getJustChangedEvents(userId: string) {
+    const fiveMinBefore = new Date()
+    fiveMinBefore.setMinutes(fiveMinBefore.getMinutes() - 5)
+
+    const params = {
+      calendarId: 'primary',
+      timeMin: new Date().toISOString(),
+      updatedMin: fiveMinBefore.toISOString(),
+      showDeleted: false,
+    }
+
+    logger.debug('Getting recently changed events', { userId, ...params })
+    const response = await this.client.events.list(params)
+    const items = response.data.items || []
+    logger.info('Got recently changed events', { count: items.length, userId })
+
+    return items
+  }
 }
