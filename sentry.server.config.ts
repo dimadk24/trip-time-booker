fb3bdd0a9ff6696ae2e847b35249e4b9a2e359fb
Dima Krutolianov <DimaKrutDK@ya.ru>
2023-07-04 22:04:38 +0200

    chore: setup sentry profiling

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -3,6 +3,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs'
+import { ProfilingIntegration } from '@sentry/profiling-node'
 import { frontendEnv } from './src/config/frontend-env'
 // during build nextjs imports this file in frontend
 // therefore need to use frontend env, not backend
@@ -13,6 +14,12 @@ if (frontendEnv.NEXT_PUBLIC_SENTRY_DSN)
 
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1,
+
+    profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
+    integrations: [
+      // Add profiling integration to list of integrations
+      new ProfilingIntegration(),
+    ],
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: frontendEnv.NEXT_PUBLIC_SERVER_SENTRY_DEBUG,
