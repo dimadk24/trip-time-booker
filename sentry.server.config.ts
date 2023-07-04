8d5bf594f5ad3fa5596a03cb701a3dda80883eaf
Dima Krutolianov <DimaKrutDK@ya.ru>
2023-07-04 21:37:59 +0200

    chore: set higher traces rate for sentry server

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -12,7 +12,7 @@ if (frontendEnv.NEXT_PUBLIC_SENTRY_DSN)
     dsn: frontendEnv.NEXT_PUBLIC_SENTRY_DSN,
 
     // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 0.2,
+    tracesSampleRate: 1,
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: frontendEnv.NEXT_PUBLIC_SERVER_SENTRY_DEBUG,
