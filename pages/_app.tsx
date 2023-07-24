268b4fc115e6cb746b7ca3afd7ac5c28da48a93c
Dima Krutolianov <DimaKrutDK@ya.ru>
2023-07-24 19:30:44 +0200

    feat: add favicon and app title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import React from 'react'
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import SuperTokensReact, { SuperTokensWrapper } from 'supertokens-auth-react'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
@@ -16,6 +17,30 @@ if (typeof window !== 'undefined') {
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <title>Trip time booker</title>
+      </Head>
       <SuperTokensWrapper>
         <QueryClientProvider client={queryClient}>
           <Component {...pageProps} />
