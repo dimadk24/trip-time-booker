1901e940f6fbb5f1740ac4435ca9ff16bcf774c0
Dima Krutolianov <DimaKrutDK@ya.ru>
2023-07-24 07:53:27 +0200

    chore: add link to logo icon creators

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import { SessionAuth } from 'supertokens-auth-react/recipe/session'
 import { Header } from '@/components/header'
 import { MainApp } from '@/components/main-app'
+import { Footer } from '@/components/footer'
 
 export default function Home() {
   return (
     <SessionAuth>
       <Header />
       <MainApp />
+      <Footer />
     </SessionAuth>
   )
 }
