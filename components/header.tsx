309ff886873af3bfbf5bc26e7e2492ebbc8979f4
Dima Krutolianov <DimaKrutDK@ya.ru>
2023-07-24 19:28:47 +0200

    feat: add user dropdown and logout ability

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,8 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-
+import { Dropdown } from 'flowbite-react'
 import Image from 'next/image'
 import { useSessionContext } from 'supertokens-auth-react/recipe/session'
-import { signOut } from 'supertokens-web-js/recipe/thirdparty'
 import logo from './icons8-delivery-time-100.png'
+import { logOut } from '@/src/frontend/user-utils'
 
 export function Header() {
   const session = useSessionContext()
@@ -13,10 +12,6 @@ export function Header() {
   }
 
   const { userId } = session
-
-  const onLogOut = async () => {
-    await signOut()
-  }
 
   return (
     <header className="bg-white border-b-2 border-gray-300 p-4 flex items-center justify-between">
@@ -28,41 +23,10 @@ export function Header() {
         />
         <span className="text-gray-800 text-lg">Trip time booker</span>
       </div>
-      <div className="flex items-center">
-        <span className="text-gray-600 mr-2">{userId}</span>
-        <div className="relative">
-          <button className="text-gray-600 focus:outline-none">
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M19 9l-7 7-7-7"
-              ></path>
-            </svg>
-          </button>
-          <div className="hidden absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-lg shadow-lg">
-            <a
-              href="#"
-              className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-            >
-              Relogin
-            </a>
-            <button
-              onClick={onLogOut}
-              className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-            >
-              Sign out
-            </button>
-          </div>
-        </div>
-      </div>
+
+      <Dropdown inline label={userId}>
+        <Dropdown.Item onClick={logOut}>Sign out</Dropdown.Item>
+      </Dropdown>
     </header>
   )
 }
