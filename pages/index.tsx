02d0ce76e38cba42fdf681c38e22a2d1990d4f1d
Dima Krutolianov <DimaKrutDK@ya.ru>
2023-07-24 19:29:27 +0200

    feat: add ability to logout/relogin by url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import { SessionAuth } from 'supertokens-auth-react/recipe/session'
 import { Header } from '@/components/header'
 import { MainApp } from '@/components/main-app'
 import { Footer } from '@/components/footer'
+import { logOut } from '@/src/frontend/user-utils'
 
 export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    if ('relogin' in router.query) {
+      logOut()
+    }
+  }, [router])
+
   return (
     <SessionAuth>
       <Header />
